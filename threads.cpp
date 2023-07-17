4a7688f2ed2bbc851342ec665c91e24784dd0a19
bmadeshw <139757291+bmadeshw@users.noreply.github.com>
2023-07-17 15:15:42 +0530

    Create threads.cpp
    Basic cpp program implemented using STL and thread.
    
    created a vector filled with few values and tried to display the values faster using multiple threads

---
+++ threads.cpp
@@ -0,0 +1,26 @@
+//C++ program 
+// Threads and STL
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+using namespace std;
+
+void print(int n, const std::string &str)  {
+  std::string msg = std::to_string(n) + " : " + str;
+  std::cout << msg  << std::endl;
+}
+	 
+int main() {
+  std::vector<std::string> s = {      "Apple",      "Orange",      "Mango",      "Grape"  };
+  std::vector<std::thread> threads;
+ 
+  for (int i = 0; i < s.size(); i++) {
+	threads.push_back(std::thread(print, i, s[i]));
+  }
+	 
+  for (auto &th : threads) {
+	th.join();
+  }
+  return 0;
+}
